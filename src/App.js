4af2a57ceb6999fc36357518b11de3100258b639
Marc Ancajas <mancajas@outlook.com>
2021-11-20 18:48:38 +0800

    build fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+import { withAuthenticator } from '@aws-amplify/ui-react'
 import '@aws-amplify/ui-react/styles.css';
 
 function App() {
